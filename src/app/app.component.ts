c7683e4a8ad0e78d1678439a37ea651013beae1c
ruslan <ruslan.kurtianyk@newfireglobal.com>
2024-10-29 00:38:17 +0200

    feat: add background image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,75 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  animations: [],
 })
 export class AppComponent {
+  private coefficient = 3;
   title = 'picture-edit';
+
+  showWorkArea = false;
+  backgroundHeight = 1760 / this.coefficient;
+  backgroundWidth = 2200 / this.coefficient;
+  backgroundImgUrl: string = '';
+  backgroundImageSize = 100;
+  backgroundImagePositionX = 0;
+  backgroundImagePositionY = 0;
+
+  onAddBackground() {
+    this.showWorkArea = true;
+  }
+
+  uploadBackground(event: any) {
+    const files = event.target.files as FileList;
+
+    if (files.length > 0) {
+      const _file = URL.createObjectURL(files[0]);
+      this.backgroundImgUrl = _file;
+      // this.resetInput();
+    }
+  }
+
+  zoomBackgroundIn() {
+    this.backgroundImageSize = this.backgroundImageSize + 1;
+  }
+
+  zoomBackgroundOut() {
+    this.backgroundImageSize = this.backgroundImageSize - 1;
+  }
+
+  moveBackgroundLeft() {
+    this.backgroundImagePositionX = this.backgroundImagePositionX - 1;
+  }
+  moveBackgroundRight() {
+    this.backgroundImagePositionX = this.backgroundImagePositionX + 1;
+  }
+  moveBackgroundTop() {
+    this.backgroundImagePositionY = this.backgroundImagePositionY - 1;
+  }
+  moveBackgroundBottom() {
+    this.backgroundImagePositionY = this.backgroundImagePositionY + 1;
+  }
 }
